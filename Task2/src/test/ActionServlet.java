5183626186f644d89aba9a921dc86a61e881a402
Gautham Ravivarma <catchgautham@gmail.com>
2018-03-12 01:08:03 -0700

    Update ActionServlet.java

--- Task2/src/test/ActionServlet.java
+++ Task2/src/test/ActionServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *  * Servlet implementation class ActionServlet  
+ * Â * Servlet implementation class ActionServlet Â 
  */
 
 public class ActionServlet extends HttpServlet {
@@ -25,55 +25,32 @@ public class ActionServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		/*String name = null;
-		name = "Hello " + request.getParameter("user");
-		if (request.getParameter("user").toString().equals("")) {
-			name = "Hello User";
-		}*/
 		response.setContentType("text/plain");
 		response.setCharacterEncoding("UTF-8");
-		
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/persondata", "root", "root");
 			Statement stmt = con.createStatement();
-
 			String keyWord = request.getParameter("keyWord");
 
 			ResultSet rs = stmt.executeQuery("SELECT * from persondata\n" + "WHERE (name LIKE '%" + keyWord + "%' OR\n"
 					+ "       address LIKE '%" + keyWord + "%' OR\n" + "       phno LIKE '%" + keyWord + "%' OR\n"
 					+ "       salary LIKE '%" + keyWord + "%' OR\n" + "       pension LIKE '%" + keyWord + "%');");
 
-			//PrintWriter pw = response.getWriter();
-			//response.setContentType("text/html");
-
-			//StringBuilder sb = new String
-			//pw.print("<font face='verdana'>");
 			if (!rs.isBeforeFirst()) { // check if ResultSet is null
 				response.getWriter().write("No match found");
-				//pw.println("No match found");
 			}
 			while (rs.next()) {
-				
-				response.getWriter().write("Name: " + rs.getString(1) + ";  " + "Address: " + rs.getString(2) + ";  " + "Phno: "
-						+ rs.getString(3) + ";  " + "Salary: " + rs.getInt(4) + ";  " + "Pension: " + rs.getInt(5) + ";  ");
-				
+
+				response.getWriter()
+						.write("Name: " + rs.getString(1) + ";  " + "Address: " + rs.getString(2) + ";  " + "Phno: "
+								+ rs.getString(3) + ";  " + "Salary: " + rs.getInt(4) + ";  " + "Pension: "
+								+ rs.getInt(5) + ";  ");
 			}
-			//pw.print("</font>");
-
-			//pw.close();
-
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
-		
-		
-		
-		
-		
-		//response.getWriter().write(name);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
